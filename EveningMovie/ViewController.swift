df56b6f8f0a2955c03dc438e269accb30ea96c70
Alex Schnapps <6kwalitet@gmail.com>
2023-04-06 22:13:24 +0400

    Init

---
+++ EveningMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EveningMovie
+//
+//  Created by Alex Schnapps on 06.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
