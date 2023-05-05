e4ba88bc6d898127b9884ac04282363cfd65b46e
Alex Schnapps <6kwalitet@gmail.com>
2023-05-05 15:46:49 +0400

    add label & VC settings in descriptionVC

--- EveningMovie/DescriptionMovieViewController.swift
+++ EveningMovie/DescriptionMovieViewController.swift
@@ -9,35 +9,78 @@ import UIKit
 import SnapKit
 
 class DescriptionMovieViewController: UIViewController {
-
-    private lazy var discriptionStackView: UIStackView = {
+    
+    private lazy var descriptionScroll: UIScrollView = {
+        var scrollView = UIScrollView()
+        scrollView = UIScrollView(frame: self.view.bounds)
+        scrollView.addSubview(descriptionStackView)
+        scrollView.contentSize = self.descriptionStackView.bounds.size
+        return scrollView
+    }()
+    
+    private lazy var descriptionStackView: UIStackView = {
         let stackView = UIStackView()
+        stackView.backgroundColor = .lightGray
         stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.layer.masksToBounds = true
+        stackView.layer.cornerRadius = 10
+        
         return stackView
     }()
     
     private lazy var descriptionImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "AppIcon")
-        imageView.layer.cornerRadius = 10
         imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 10
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
+    private lazy var movieTiteleLabel: UILabel = {
+        var label = UILabel()
+        label.text = "safafnofqofof ofnoifoqifoqifnoqin oqifnioeqfnqoifnoqfn ufucucuciyfiugugou ufigougogiugigigg giugugogouhouhougu uigiugiugiug hiugiugiugiu hiugiugiugiugguig iugiuguigiugiugiug"
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 14, weight: .semibold)
+        label.backgroundColor = .lightGray
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 10
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        settingsVC()
         addSubviews()
         addConstraints()
     }
     
     private func addConstraints() {
+        descriptionStackView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(10)
+            $0.width.equalTo(view.bounds.width - 20)
+        }
         descriptionImageView.snp.makeConstraints {
-            $0.leading.trailing.top.bottom.equalToSuperview().inset(10)
+            $0.top.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(10)
+        }
+        movieTiteleLabel.snp.makeConstraints {
+            $0.top.equalTo(descriptionImageView.snp.bottom).offset(10)
+            $0.leading.equalToSuperview().inset(10)
+            $0.bottom.lessThanOrEqualToSuperview().inset(10)
         }
     }
     
     private func addSubviews() {
-        view.addSubview(discriptionStackView)
+        descriptionStackView.addArrangedSubview(descriptionImageView)
+        descriptionStackView.addArrangedSubview(movieTiteleLabel)
+        view.addSubview(descriptionScroll)
+    }
+    
+    private func settingsVC() {
+        self.title = "Description"
+        self.view.backgroundColor = .white
     }
 }
